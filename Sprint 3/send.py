73e0059e68e892b66b136c90da5905b0bb3dcb93
Mahegit45 <mahesh522018_it@mepcoeng.ac.in>
2022-11-18 20:54:56 +0530

    Sprint 4
    Co-Authored-By: SriHari456 <113231115+SriHari456@users.noreply.github.com>
    Co-Authored-By: krish766 <111215632+krish766@users.noreply.github.com>
    Co-Authored-By: shalman100 <113233268+shalman100@users.noreply.github.com>

---
+++ Sprint 3/send.py
@@ -0,0 +1,18 @@
+
+
+
+from flask import Flask
+from flask_mail import Mail, Message
+
+sender_email="maheshgurulakshmi@gmail.com"
+        rec_email= session['username']
+        password='xxxxxxxxxxxxxxxxx'
+        message='ALERT MESSAGE FROM EXPENSE TRACKER !PLEASE CHECK  YOUR DAILY EXPENSE YOUR DAILY EXPENSE IS CROSS YOUR MONTHLY LIMIT. UPDATE YOUR MONTHLY LIMIT AND AVOID YOUR UNWANTED DAILY EXPENSE'
+        # soup=BeautifulSoup(html_data,'html.parser')      
+        print(message)
+        server=smtplib.SMTP('smtp.gmail.com',587)
+        server.starttls()
+        server.login(sender_email,password)
+        print("login successfully")
+        server.sendmail(sender_email,rec_email,message)
+        print("email has been sende successfully")
